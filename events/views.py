57d165a88adf1bde24392bc882c8c021981a50d2
jack-khalif <mugwewaithaka@2>
2026-03-10 00:43:57 +0300

    views.py to see where the ticket comes to view availability of event

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+class EventListAPIView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+'''
+This is a simple API view that lists all events.
+- generics.ListAPIView → a built-in view that provides a read-only endpoint to list all instances of a model.
+- queryset → defines which data to retrieve (all events).
+- serializer_class → specifies how to convert the Event objects into JSON format for the API response.
+'''
