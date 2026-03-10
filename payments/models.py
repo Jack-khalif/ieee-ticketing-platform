85a0944caae6b4cff5f3c0c710c0b1eb68c9f32c
jack-khalif <mugwewaithaka@2>
2026-03-10 09:32:38 +0300

    migrate the payments model: added payments to admin dashboard

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from tickets.models import Ticket
+
+
+class Payment(models.Model):
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+
+    transaction_id = models.CharField(max_length=200, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment {self.id} - {self.status}"
+    
+'''
+THis model represents a mobile money transaction
+'''
